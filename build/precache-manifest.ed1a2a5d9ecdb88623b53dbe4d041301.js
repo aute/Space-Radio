df97f7277e0126d269704e2d738ff0fdc84b8922
aute <no.at@live.com>
2018-11-25 16:33:19 +0800

    init

---
+++ build/precache-manifest.ed1a2a5d9ecdb88623b53dbe4d041301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb4368b2f60c615e4a9b",
+    "url": "/static/js/main.eb4368b2.chunk.js"
+  },
+  {
+    "revision": "c543db7aafcc8162b943",
+    "url": "/static/js/1.c543db7a.chunk.js"
+  },
+  {
+    "revision": "eb4368b2f60c615e4a9b",
+    "url": "/static/css/main.b76b6383.chunk.css"
+  },
+  {
+    "revision": "29ec63ec27c18112789aa94fb22b0581",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import screenfull from "screenfull";
+import "./App.css";
+import { GetDistance } from "./utils";
+
+// eslint-disable-next-line
+const geolocation = new qq.maps.Geolocation(
+  "MPFBZ-TPZW4-AWOU3-XG2RL-3VE47-MSFIE",
+  "Outer Space Radio"
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hour: 0,
+      loca_lat: 0,
+      loca_lng: 0,
+      iss_lat: 0,
+      iss_lng: 0,
+      distance: ''
+    };
+  }
+  componentDidMount() {
+    this.getHour();
+    this.getLoca();
+    this.getIssPosition();
+    this.background_timer = setInterval(() => {
+      this.getHour();
+    }, 1000 * 60 * 60);
+    this.getIssPosition_timer = setInterval(() => {
+      this.getIssPosition();
+    }, 1000);
+  }
+  getHour = () => {
+    let now = new Date();
+    this.setState({
+      hour: now.getMinutes() > 50 ? now.getHours() + 1 : now.getHours()
+    });
+  };
+  getLoca = () => {
+    geolocation.getLocation(payload => {
+      this.setState({
+        loca_lat: payload.lat,
+        loca_lng: payload.lng
+      });
+    });
+  };
+  getIssPosition = () => {
+    fetch("http://api.open-notify.org/iss-now.json").then(res => {
+      res.json().then(data => {
+        this.setState(
+          {
+            iss_lat: data.iss_position.latitude,
+            iss_lng: data.iss_position.longitude
+          },
+          () => {
+            this.setState(
+              {
+                distance: GetDistance(
+                  this.state.loca_lat,
+                  this.state.loca_lng,
+                  this.state.iss_lat,
+                  this.state.iss_lng,
+                )
+              }
+            )
+          }
+        );
+      });
+    });
+  };
+  screenfullSwitch = () => {
+    if (screenfull.enabled) {
+      screenfull.toggle(this.refs.App);
+    } else {
+      alert("screen full error");
+    }
+  };
+  render() {
+    return (
+      <div className={["App", `sky-gradient-${this.state.hour}`].join(" ")} ref="App">
+        <div className="container" onDoubleClick={this.screenfullSwitch}>
+          <h1>
+            {this.state.distance}
+          </h1>
+        </div>
+      </div>
+    );
+  }
+  componentWillUnmount() {
+    this.background_timer && clearInterval(this.background_timer);
+    this.getIssPosition_timer && clearInterval(this.getIssPosition_timer);
+  }
+}
+
+export default App;
