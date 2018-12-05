cf12a0bf2e10e6d2052b0dac990cf281307a36e5
aute <no.at@live.com>
2018-12-05 10:19:23 +0800

    fix

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,38 +1,51 @@
 import React, { Component } from "react";
 
 export default class Player extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      audioStar: false,
+      audioStar: false
     };
   }
   componentDidUpdate(prevProps) {
     if (prevProps.audioStar !== this.props.audioStar && this.props.audioStar) {
-      this.audioStart()
+      this.audioStart();
     }
     if (prevProps.distance !== this.props.distance) {
-      let volume = 2400 - this.props.distance > 1 ? (this.props.distance / 2400) * (this.props.distance / 2400) * (this.props.distance / 2400) * (this.props.distance / 2400) : 1
-      volume = volume * 0.05
-      this.backgroundAudio_main.volume = volume
+      let volume =
+        2400 - this.props.distance > 1
+          ? Math.pow((this.props.distance / 2400),4)
+          : 1;
+      volume = volume * 0.05;
+      this.backgroundAudio_main.volume = volume;
     }
   }
   componentDidMount() {
     this.backgroundAudio_main.volume = 0.05;
   }
   audioStart = () => {
-    this.backgroundAudio_main.play()
-  }
+    this.backgroundAudio_main.play();
+  };
   render() {
     return [
-      <audio
-        key="audio"
-        src={"/Pinknoise.mp3"}
-        ref={node => {
-          this.backgroundAudio_main = node;
-        }}
-        loop
-      />
+      <div>
+        <audio
+          key="audio"
+          src={"/Pinknoise.mp3"}
+          ref={node => {
+            this.audio = node;
+          }}
+          loop
+        />
+        <audio
+          key="backgroundAudio_main"
+          src={"/Pinknoise.mp3"}
+          ref={node => {
+            this.backgroundAudio_main = node;
+          }}
+          loop
+        />
+      </div>
     ];
   }
 }
