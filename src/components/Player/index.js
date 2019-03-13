ee3f5c8b19160950445b8350aaaa76cde065f57a
aute <no.at@live.com>
2019-03-13 16:31:07 +0800

    palyer 重构

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -7,101 +7,77 @@ export default class Player extends Component {
     this.state = {
       audioStar: false,
       markDistance: 2300,
+      ISSPassing: false,
     };
-    this.backgroundAudio = new Tone.Noise("pink")
+    this.backgroundAudio = new Tone.Noise("pink").toMaster()
     this.musicAudio = new Tone.Player("./1.mp3").toMaster()
   }
   componentDidMount() {
-    //this.setBackgroundAudioVolume(0.2)
   }
   componentDidUpdate(prevProps) {
     this.audioController(
-      prevProps.audioStar,
-      this.props.audioStar,
       prevProps.distance,
       this.props.distance,
       this.state.markDistance
     );
+    if (prevProps.audioStar !== this.props.audioStar && this.props.audioStar) {
+      this.backgroundAudioInitStart()
+    }
   }
-  audioStart = () => {
-    this.audio.play()
-    this.backgroundAudio.start()
+  backgroundAudioInitStart = () => {
     this.backgroundAudio.volume.value = -1
-    this.backgroundAudio.toMaster()
-    this.musicAudio.start()
-    //this.backgroundAudio_main.play();
-    //this.backgroundAudio_paint.play();
-    // setInterval(() => {
-    //   let currentTime = Math.ceil(Math.random() * (this.backgroundAudio_paint.duration - 5))
-    //   this.backgroundAudio_paint.currentTime = currentTime
-    // }, 1000 * 5);
+    this.backgroundAudio.start()
   };
   setBackgroundAudioVolume = (value) => {
     if (value < -500) {
       value =  -500
     }
     this.backgroundAudio.volume.value = value
-    //this.backgroundAudio_main.volume = value;
-    //this.backgroundAudio_paint.volume = value;
   }
+  setMusicAudioVolume = (value) => {
+    this.musicAudio.volume.value = value
+  }
+  ISSover = () => {
+    this.musicAudio.start()
+  }
+  ISSmove = (Distance) => {
+    let backgroundAudioVolume = -(Math.pow(this.state.markDistance / Distance, 9))
+    let musicAudioVolume = -501 - backgroundAudioVolume
+    this.setBackgroundAudioVolume(backgroundAudioVolume)
+    this.setMusicAudioVolume(musicAudioVolume)
+  }
+  ISSout = () => {
+    this.setBackgroundAudioVolume(-1)
+    this.musicAudio
+  }
+
   audioController = (
-    prevPropsAudioStar,
-    thisPropsAudioStar,
     prevPropsDistance,
     thisPropsDistance,
     markDistance,
   ) => {
-    if (prevPropsAudioStar !== thisPropsAudioStar && thisPropsAudioStar) {
-      this.audioStart();
-    }
-
-    // passing
     if (prevPropsDistance !== thisPropsDistance && markDistance - thisPropsDistance > 0) {
-      
-      let audioVolume = 1 - Math.pow(thisPropsDistance / markDistance, 2)
-      this.audio.play()
-      this.audio.volume = audioVolume;
-      let backgroundAudioVolume =  -(Math.pow(markDistance / thisPropsDistance,9))
-      this.setBackgroundAudioVolume(backgroundAudioVolume)
+      if (!this.state.ISSPassing) {
+        this.ISSover()
+        this.setState({
+          ISSPassing:true
+        })
+      }
+      this.ISSmove()
     }
-
+    // Iss Out
     if (prevPropsDistance !== thisPropsDistance && markDistance - thisPropsDistance <= 0) {
-      //let backgroundAudioVolume = 1;
-      //backgroundAudioVolume = backgroundAudioVolume * 0.2;
-      let audioVolume = 0;
-      this.audio.pause()
-      //this.setBackgroundAudioVolume(backgroundAudioVolume)
-      this.audio.volume = audioVolume;
+      if (!this.state.ISSPassing) {
+        this.ISSout()
+        this.setState({
+          ISSPassing:false
+        })
+      }
     }
   };
   render() {
     return (
       <div>
-        <audio
-          autoPlay={false}
-          src={"./1.mp3"}
-          ref={node => {
-            this.audio = node;
-          }}
-          loop
-        />
-        <audio
-          src={"./b.mp3"}
-          ref={node => {
-            this.backgroundAudio_main = node;
-          }}
-          onLoadedMetadata={e => {
-            this.props.onCanPlay(e);
-          }}
-          loop
-        />
-        <audio
-          src={"./b.mp3"}
-          ref={node => {
-            this.backgroundAudio_paint = node;
-          }}
-          loop
-        />
       </div>
     );
   }
